2bf923d2bd4399a78c0e67313c18da6b6fd814db
SBesedin <sbesedin@gorvodokanal.com>
2020-07-06 15:58:42 +0700

    countries section added

--- project/src/js/script.js
+++ project/src/js/script.js
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 $(document).ready(function(){
-    $('.carousel_inner').slick({
+    $('.carousel_inner_styles').slick({
         infinite: false,
         slidesToShow: 4,
         slidesToScroll: 4,
@@ -35,16 +35,45 @@ $(document).ready(function(){
             {
                 breakpoint: 768,
                 settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2
+                    slidesToShow: 2,
+                    slidesToScroll: 2
                 }
               },
               {
                 breakpoint: 576,
                 settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
-              },]
+            },]
+    });
+    $('.carousel_inner_countries').slick({
+        infinite: false,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/arrow-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/arrow-right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },]
       });
   });
