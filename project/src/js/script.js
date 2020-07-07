eadf2dc6932f2e65348618abcd4a0d6423948a45
SBesedin <sbesedin@gorvodokanal.com>
2020-07-07 13:13:25 +0700

    adaptation ready

--- project/src/js/script.js
+++ project/src/js/script.js
@@ -57,22 +57,22 @@ $(document).ready(function(){
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
+                    slidesToShow: 4,
+                    slidesToScroll: 4
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 3,
+                    slidesToScroll: 3
                 }
             },
             {
                 breakpoint: 576,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToShow: 2,
+                    slidesToScroll: 2
                 }
             },]
       });
